4948b5b269d2a4c0df0c1e5c658451073b39388d
Arjan Molenaar <gaphor@gmail.com>
2019-12-07 15:25:11 +0100

    Day 7 part 2

--- 07/day7.py
+++ 07/day7.py
@@ -1,3 +1,4 @@
+import itertools
 
 def parse_memory(line):
     return list(map(int, line.split(",")))
@@ -7,7 +8,8 @@ def memory():
         line = input.readline()
 
     return parse_memory(line)
-139629729
+
+
 def param(codes, index_or_val, mode):
     if mode == 1:
         return index_or_val
@@ -24,10 +26,9 @@ def write(codes, index_or_val, val, mode):
         raise ValueError(f"Invalid mode {mode}")
 
 
-def intcode_computer(initial_codes, inputs):
+def intcode_computer(initial_codes):
     codes = list(initial_codes)
     addr = 0
-    outputs = []
     while True:
         instr = codes[addr]
         arg3_mode = instr // 10000
@@ -51,13 +52,14 @@ def intcode_computer(initial_codes, inputs):
             addr += 4
         elif opcode == 3: # input
             _, i = codes[addr:addr+2]
-            p = inputs.pop()
+            p = yield
+            assert p is not None
             write(codes, i, p, arg1_mode)
             addr += 2
         elif opcode == 4: # output
             _, o = codes[addr:addr+2]
             p = param(codes, o, arg1_mode)
-            outputs.append(p)
+            yield p
             addr += 2
         elif opcode == 5: # jump-if-true
             _, i, o = codes[addr:addr+3]
@@ -89,15 +91,17 @@ def intcode_computer(initial_codes, inputs):
             addr += 4
         else:
             raise ValueError(f"Invalid code {opcode} at addr {addr}")
-    return outputs
 
 
 def amplifiers(mem, inputs):
-    out = [0]
+    out = 0
     for i in inputs:
-        out = intcode_computer(list(mem), out + [i])
-        assert len(out) == 1
+        c = intcode_computer(list(mem))
+        next(c)
+        c.send(i)
+        out = c.send(out)
     return out
+
 
 def gen_inputs(inputs=()):
     if len(inputs) == 5:
@@ -106,11 +110,12 @@ def gen_inputs(inputs=()):
         if i not in inputs:
             yield from gen_inputs(inputs + (i,))
 
+
 def part1():
     mem = memory()
     out = []
     for inputs in gen_inputs():
-        out.append(amplifiers(mem, inputs)[0])
+        out.append(amplifiers(mem, inputs))
     return max(out)
 
 
@@ -119,30 +124,53 @@ def gen_inputs2(inputs=()):
         yield inputs
     for i in range(5, 10):
         if i not in inputs:
-            yield from gen_inputs(inputs + (i,))
+            yield from gen_inputs2(inputs + (i,))
+
+
+def amplifiers2(mem, inputs):
+    def init(i):
+        c = intcode_computer(list(mem))
+        next(c) # init
+        c.send(i) # read first input
+        return c
+
+    amps = [init(i) for i in inputs]
+
+    signal = 0
+    output = 0
+    for amp in itertools.cycle(amps):
+        try:
+            out = amp.send(signal)
+        except StopIteration:
+            return output
+        if out is not None:
+            signal = out
+            if amp is amps[-1]:
+                output = signal
+            continue # allow to send signal into next amp
 
 
 def part2():
     mem = memory()
     out = []
     for inputs in gen_inputs2():
-        out.append(amplifiers(mem, inputs)[0])
+        out.append(amplifiers2(mem, inputs))
     return max(out)
 
 
 def test_example1():
     mem = parse_memory("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
-    assert amplifiers(mem, (4, 3, 2, 1, 0)) == [43210]
+    assert amplifiers(mem, (4, 3, 2, 1, 0)) == 43210
 
 
 def test_example2():
     mem = parse_memory("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")
-    assert amplifiers(mem, (0, 1, 2, 3, 4)) == [54321]
+    assert amplifiers(mem, (0, 1, 2, 3, 4)) == 54321
 
 
 def test_example3():
     mem = parse_memory("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")
-    assert amplifiers(mem, (1,0,4,3,2)) == [65210]
+    assert amplifiers(mem, (1,0,4,3,2)) == 65210
 
 
 def test_gen_inputs():
@@ -151,7 +179,12 @@ def test_gen_inputs():
 
 def test_feedback_example1():
     mem = parse_memory("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5")
-    assert amplifiers(mem, (9,8,7,6,5)) == [139629729]
+    assert amplifiers2(mem, (9,8,7,6,5)) == 139629729
+
+
+def test_feedback_example2():
+    mem = parse_memory("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10")
+    assert amplifiers2(mem, (9,7,8,5,6)) == 18216
 
 
 if __name__ == "__main__":
