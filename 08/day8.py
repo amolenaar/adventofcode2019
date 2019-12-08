91fbc0f8a94d79b83535783265253d6025d959dd
Arjan Molenaar <gaphor@gmail.com>
2019-12-08 14:22:09 +0100

    Day 8, part 2

--- 08/day8.py
+++ 08/day8.py
@@ -1,5 +1,5 @@
 
-from itertools import zip_longest
+from itertools import dropwhile, zip_longest
 
 
 def grouper(iterable, n, fillvalue=None):
@@ -18,6 +18,14 @@ def count(l, m):
     return sum(1 for v in l if v == m)
 
 
+def align_pixels(layers):
+    return zip(*layers)
+
+
+def find_color(pixels):
+    return next(dropwhile(lambda p: p == "2", pixels))
+
+
 def test_grouper():
     gr = grouper("abcdefghijklmnopqr", 6)
     assert "".join(next(gr)) == "abcdef"
@@ -30,7 +38,19 @@ def test_layers():
 
 
 def test_count_zeros():
-    assert count_zeros("1203405607") == 3
+    assert count("1203405607", "0") == 3
+
+
+def test_align_pixels():
+    data = "0222112222120000"
+    pixels = align_pixels(grouper(data, 4))
+    assert next(pixels) == ("0", "1", "2", "0")
+    assert next(pixels) == ("2", "1", "2", "0")
+
+    
+def test_find_color():
+    assert find_color(("0", "1", "2", "0")) == "0"
+    assert find_color(("2", "2", "2", "1")) == "1"
 
 
 def part1():
@@ -38,6 +58,14 @@ def part1():
     return count(layer, "1") * count(layer, "2")
 
 
+def part2():
+    image = map(find_color, align_pixels(grouper(data(), 25*6)))
+    for line in grouper(image, 25):
+        print("".join(line).replace("0", " "))
+
+
 if __name__ == "__main__":
     print("Part 1:", part1())
+    part2()
+    print("Part 2: PHPEU")
 
